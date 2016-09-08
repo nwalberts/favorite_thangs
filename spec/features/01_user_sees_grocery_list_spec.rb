fe77d05f506ded2a6c6ee9cc36a73c58f4446095
Nick Alberts <nwalberts5@gmail.com>
2016-09-08 11:38:57 -0400

    initial commit

---
+++ spec/features/01_user_sees_grocery_list_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+feature "user sees a list of favorite things" do
+  scenario "user views favorites" do
+    CSV.open("favorites_list.csv", "a", headers: true) do |file|
+      file.puts(["pokemon"])
+      file.puts(["sleep"])
+      file.puts(["green smoothies"])
+    end
+
+    visit "/favorites"
+    expect(page).to have_content("pokemon")
+    expect(page).to have_content("sleep")
+    expect(page).to have_content("green smoothies")
+  end
+
+  scenario "user sees all favorites at root path" do
+    CSV.open("favorites_list.csv", "a", headers: true) do |file|
+      thing = "friendship"
+      file.puts([thing])
+    end
+    visit "/"
+
+    expect(page).to_not have_content("friendship")
+  end
+end
